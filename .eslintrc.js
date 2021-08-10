24067afcc1e5365aa74efbeeb177d383a142a5e8
Mr nUUb <josef.schabasser@gmx.at>
2021-08-11 00:03:56 +0200

    add typescript, prettier, eslint, add yarn SDKs, add configs

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'import', 'tsdoc', 'prettier'],
+  rules: {
+    '@typescript-eslint/no-shadow': 'error',
+    'arrow-body-style': ['error', 'as-needed'],
+    'import/no-duplicates': 'error',
+    'indent-legacy': 'off',
+    indent: 'off',
+    'jsx-quotes': ['error', 'prefer-single'],
+    'linebreak-style': ['error', 'unix'],
+    'max-len': ['error', { code: 100, ignoreUrls: true, ignoreComments: true }],
+    'no-undef': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'no-var': ['error'],
+    'object-shorthand': 'error',
+    'prefer-const': ['error', { destructuring: 'any' }],
+    quotes: 'off',
+    semi: ['error', 'never'],
+    'no-param-reassign': ['error'],
+    'tsdoc/syntax': 'warn',
+  },
+}
