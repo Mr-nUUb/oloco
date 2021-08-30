d84ce263ee5a99cd5cf68e12ee4c748628e313e3
Mr nUUb <josef.schabasser@gmx.at>
2021-08-30 21:18:44 +0200

    add ability to query current config

--- packages/ek-cli/src/commands/config.ts
+++ packages/ek-cli/src/commands/config.ts
@@ -1,7 +1,7 @@
 import { Argv } from 'yargs'
 
 export const command = 'config <action>'
-export const describe = 'Create or delete the user configuration file.'
+export const describe = 'Query configuration values or delete the current configuration file.'
 
 export const builder = (yargs: Argv): Argv =>
   yargs.commandDir('config', { extensions: ['js', 'ts'] })
---
+++ packages/ek-cli/src/commands/config/get.ts
@@ -0,0 +1,17 @@
+import fs from 'fs'
+import { Arguments, Argv } from 'yargs'
+import { Config } from '../../config'
+
+export const command = 'get [entry]'
+export const describe = 'Query an entry from the current configuration.'
+
+export const builder = (yargs: Argv): Argv =>
+  yargs.positional('port', {
+    type: 'string',
+    describe: 'The entry to query.',
+  })
+
+export const handler = (yargs: Arguments): void => {
+  const entry = yargs.entry as string
+  console.log(Config.get(entry))
+}
