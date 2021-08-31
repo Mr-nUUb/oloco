6874af4969f0d4fe6ca89fd243ee98ec0e571a78
Mr nUUb <josef.schabasser@gmx.at>
2021-08-31 22:48:36 +0200

    wording

--- packages/ek-cli/src/commands/config.ts
+++ packages/ek-cli/src/commands/config.ts
@@ -1,7 +1,7 @@
 import { Argv } from 'yargs'
 
 export const command = 'config <action>'
-export const describe = 'Query configuration values or delete the current configuration file.'
+export const describe = 'Get or set configuration values or delete the current configuration file.'
 
 export const builder = (yargs: Argv): Argv =>
   yargs.commandDir('config', { extensions: ['js', 'ts'] })
