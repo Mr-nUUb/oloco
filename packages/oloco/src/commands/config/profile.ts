a3d2ac32a1b4aeb06c846779fa68f31cceae77a6
Mr nUUb <josef.schabasser@gmx.at>
2021-10-12 20:46:47 +0200

    create and delete custom profiles

---
+++ packages/oloco/src/commands/config/profile.ts
@@ -0,0 +1,7 @@
+import { Argv } from 'yargs'
+
+export const command = 'profile <action>'
+export const describe = 'Work with custom fan profiles.'
+
+export const builder = (yargs: Argv): Argv =>
+  yargs.commandDir('profile', { extensions: ['js', 'ts'] })
