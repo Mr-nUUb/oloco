1b9ef2a8554405ab0565f955c8e2187586e39780
Mr nUUb <josef.schabasser@gmx.at>
2021-10-13 00:06:04 +0200

    allow copying predefined profiles

--- packages/oloco/src/commands/config/profile/copy.ts
+++ packages/oloco/src/commands/config/profile/copy.ts
@@ -1,6 +1,10 @@
+import { fanProfileChoices, FanProfileName, FanProfilePoint } from '../../../cli.common'
 import { exit } from 'process'
 import { Arguments, Argv } from 'yargs'
-import { Config } from '../../../config'
+import { Config, CustomProfile } from '../../../config'
+import fanSilent from '../../../res/silent.json'
+import fanBalanced from '../../../res/balanced.json'
+import fanMax from '../../../res/max.json'
 
 export const command = 'copy [name] [newName]'
 export const describe = 'Copy a custom fan profile.'
@@ -19,23 +23,34 @@ export const builder = (yargs: Argv): Argv =>
 export const handler = (yargs: Arguments): void => {
   const name = yargs.name as string
   const newName = yargs.newName as string
+  const profiles = Config.get('profiles')
 
-  if (name === newName) {
+  if (name === newName || profiles.some((p) => p.name === newName)) {
     console.error(`Duplicate profile names are not allowed!`)
     exit(1)
   }
 
-  const profiles = Config.get('profiles')
   const index = profiles.findIndex((p) => p.name === name)
 
+  const profile: CustomProfile = { name: newName, profile: [] }
   if (index === -1) {
-    console.error(`Couldn't find profile "${name}"!`)
-    exit(2)
+    if (fanProfileChoices.some((f) => f === name)) {
+      const predefined = {
+        balanced: fanBalanced as FanProfilePoint[],
+        silent: fanSilent as FanProfilePoint[],
+        max: fanMax as FanProfilePoint[],
+        custom: [] as FanProfilePoint[],
+      }
+      profile.profile = predefined[name as FanProfileName]
+    } else {
+      console.error(`Couldn't find profile "${name}"!`)
+      exit(2)
+    }
+  } else {
+    profile.profile = profiles[index].profile
   }
 
   const next = profiles.length
-  const profile = profiles[index]
-  profile.name = newName
 
   try {
     Config.set(`profiles.${next}`, profile)
