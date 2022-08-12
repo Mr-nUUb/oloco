ae710eb30ce566ff065a8140f3db1997e0fd60c7
Mr_nUUb <josef.schabasser@gmx.at>
2022-08-12 23:24:49 +0200

    add index file

---
+++ packages/oloco/src/index.ts
@@ -0,0 +1,14 @@
+export { OLoCo } from './lib/oloco'
+
+export { DevicePort, FanPort, LevelData, RgbMode, RgbSpeed, TempPort } from './lib/types'
+
+export {
+  CurveData,
+  CurvePoint,
+  DeviceInformation,
+  FanData,
+  RgbColor,
+  RgbData,
+  SensorData,
+  TempData,
+} from './lib/interfaces'
