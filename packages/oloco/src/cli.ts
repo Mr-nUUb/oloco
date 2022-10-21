66fd4b9444a2f1bdcf3624ab6c4092ffe3588776
Mr_nUUb <josef.schabasser@gmx.at>
2022-10-21 15:35:09 +0200

    always scan TS and JS files

--- packages/oloco/src/cli.ts
+++ packages/oloco/src/cli.ts
@@ -1,14 +1,11 @@
 #!/usr/bin/env node
 
 import yargs from 'yargs/yargs'
-import detectTSNode from 'detect-ts-node'
 
 yargs(process.argv.slice(2))
   .scriptName('oloco')
   //.usage('Usage: $0 <command>')
-  .commandDir('commands', {
-    extensions: [detectTSNode ? 'ts' : 'js'],
-  })
+  .commandDir('commands', { extensions: ['ts', 'js'] })
   .demandCommand()
   .alias('h', 'help')
   .alias('v', 'version')
