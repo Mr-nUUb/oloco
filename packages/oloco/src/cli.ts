1c81614839c6bee42ed5243cda5e6d7f95d7afe0
Mr_nUUb <josef.schabasser@gmx.at>
2023-01-15 23:26:06 +0100

    Revert "always scan TS and JS files"
    This reverts commit 66fd4b9444a2f1bdcf3624ab6c4092ffe3588776.

--- packages/oloco/src/cli.ts
+++ packages/oloco/src/cli.ts
@@ -2,11 +2,14 @@
 
 import yargs from 'yargs/yargs'
 import { hideBin } from 'yargs/helpers'
+import detectTSNode from 'detect-ts-node'
 
 yargs(hideBin(process.argv))
   .scriptName('oloco')
   //.usage('Usage: $0 <command>')
-  .commandDir('commands', { extensions: ['ts', 'js'] })
+  .commandDir('commands', {
+    extensions: [detectTSNode ? 'ts' : 'js'],
+  })
   .demandCommand()
   .alias('h', 'help')
   .alias('v', 'version')
